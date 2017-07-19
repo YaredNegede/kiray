41703b93495caf791aadd708ac1c3ef8fdd01c77
Yared.negede <yared.negede@dventus.com>
2017-07-19 21:09:57 +0300

    item added

---
+++ src/components/component/Freelancer.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="row"> 
+	
+	<div class="col-sm-6 col-md-2  col-lg-3"> 
+		
+		<a href="#" class="thumbnail">
+			
+			<img src="../../assets/freelancer.png" alt="thumbnail"> 
+			
+		</a> 
+	</div> 
+	
+	
+	<div class="col-sm-6 col-md-2  col-lg-3"> 
+		
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-md-2  col-lg-3">A</div>
+				<div class="col-sm-6 col-md-2  col-lg-3">B</div>
+				<div class="col-sm-6 col-md-2  col-lg-3">C</div>
+				<div class="col-sm-6 col-md-2  col-lg-3">D</div>
+			</div>
+		</div>
+		 
+	</div> 
+	
+	</div>
+</template>
+
+<script>
+
+export default {
+  name: 'FreeLancer',
+  methods: {}
+}
+</script>
+<style scoped>
+	frameStyle{
+		width: 300px
+	}
+	
+  h4{
+    background-color:#c1c1c1;
+    color:white;
+    padding:5px;
+  }
+</scope>
